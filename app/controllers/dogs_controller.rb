affbdd9fc8a07fb332333f42959072f069402319
Sarah Thiery <sarahvthiery@gmail.com>
2020-12-14 17:31:22 -0500

    created dunder mifflin domain with dog and employee classes

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,25 @@
 class DogsController < ApplicationController
+
+    def index 
+        @dogs = Dog.all 
+    end 
+    
+    def new 
+        @dog = Dog.new 
+    end 
+    
+    def show
+        @dog = Dog.find(params[:id])
+    end 
+    
+    def create 
+        @dog = Dog.create(params_path(@dog))
+    end 
+    
+    private
+
+    def params_path
+        params.require(:dog).permit(:name, :age, :breed)
+    end 
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,36 @@
 class EmployeesController < ApplicationController
+
+    def index  
+        @employees = Employee.all 
+    end 
+    
+    def new 
+        @employee = Employee.new 
+    end 
+    
+    def show
+        @employee = Employee.find(params[:id])
+    end 
+    
+    def create 
+        @employee = Employee.create(params_path)
+        redirect_to employee_path(@employee)
+    end 
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+    
+    def update
+        @employee = Employee.find(params[:id])
+       
+        @employee.update(params_path)
+        redirect_to employee_path(@employee)
+    end 
+
+    private
+
+    def params_path
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end 
 end
